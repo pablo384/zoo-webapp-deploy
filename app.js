8471c771b7d91ee24c95a872d96bb124891069f1
Pablo Reinoso <pabloreinoso0@gmail.com>
2018-02-07 14:26:12 -0400

    instancia render cambiada

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const animal_routes = require('./routes/animal');
 //middleware de body-parser
 //render
 app.use(rendertron.makeMiddleware({
-  proxyUrl: 'http://104.236.237.133:8080/render',
+  proxyUrl: 'https://render-tron.appspot.com/render',
 }));
 
 
